593872cc630ea9860819dbccce54df407bfc9694
Claudia Fox <claudiafox95@icloud.com>
2019-03-13 17:24:22 +0000

    Added user to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,5 +24,12 @@ users = User.create([
     profile_picture: 'https://i.pinimg.com/280x280_RS/72/a3/51/72a3510997b041b2d01c05831e618fd2.jpg',
     description: "Hey it's Kitty here! I'm a student studying Accountancy in Newcastle. Hoping to meet some new people in my new city!"
   },
+  {
+    first_name: 'Issy',
+    last_name: 'Fox',
+    email_address: 'issyfox@icloud.com',
+    password: 'i55y',
+    profile_picture: 'https://scontent-lht6-1.xx.fbcdn.net/v/t31.0-8/13691108_117795928655673_6418123535609420222_o.jpg?_nc_cat=109&_nc_ht=scontent-lht6-1.xx&oh=5fa70467f0c67c1178b8ee46e9215585&oe=5D28A153',
+    description: "I'm Issy, 18 and studying to become a nurse. Love Sunday Lunches and funny people!"
+  },
   ])
-  

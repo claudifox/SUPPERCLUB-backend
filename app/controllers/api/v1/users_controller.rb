ce430a44c176b39a4bff9e4c87a6866ded122a77
Claudia Fox <claudiafox95@icloud.com>
2019-03-14 18:01:30 +0000

    Added first name and last name to validation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::UsersController < ApplicationController
   def validate
     @user = get_current_user
     if @user
-      render json: {email_address: @user.email_address, token: issue_token({id: @user.id}), user_id: @user.id}
+      render json: {email_address: @user.email_address, token: issue_token({id: @user.id}), first_name: @user.first_name, last_name: @user.last_name, user_id: @user.id}
     else
       render json: {error: "Your email address or password is incorrect"}, status: 401
     end
